b7165756b2470ce5416286af9c790afa1b5e5c54
SalmaaOsamaa <79584660+SalmaaOsamaa@users.noreply.github.com>
2023-12-17 01:01:34 +0200

    adding files

--- src/components/ui/SearchBox.jsx
+++ src/components/ui/SearchBox.jsx
@@ -2,34 +2,36 @@ import React,{useState,useEffect} from 'react'
 import styles from './SearchBox.module.scss'
 function SearchBox({onSearch,searchInput}) {
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions,setShowSuggestions] = useState(false)
   const [selectedSuggestion, setSelectedSuggestion] = useState(null);
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
-    if (searchInput.trim() === '') {
+    setShowSuggestions(true)
+    if (query.trim() === '') {
       setSuggestions([]);
       return;
     }
-    fetch(`https://skills-api-zeta.vercel.app/jobs/search?query=${searchInput}`)
+    fetch(`https://skills-api-zeta.vercel.app/jobs/search?query=${query}`)
       .then((response) => response.json())
       .then((data) => {
         const jobTitles = data.data.jobs.map((job) => job.attributes.title);
         setSuggestions(jobTitles);
-  
-        console.log(suggestions);
-        // setSuggestions(data.data.jobs.map((item) => item.));
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-  }, [searchInput]);
-  const handleSearch = (query) => {
-    onSearch(query);
-    setSelectedSuggestion(null);
+  }, [query]);
+  const handleSearch = (q) => {
+    setQuery(q)
+
   };
 
   const handleClick = (title) => {
-    setSelectedSuggestion(title);
-    handleSearch(title.toLowerCase()); 
+    onSearch(title.toLowerCase());
+    setQuery(title)
+    setSelectedSuggestion(title.toLowerCase())
+    setShowSuggestions(false)
   };
  
     return (
@@ -39,17 +41,17 @@ function SearchBox({onSearch,searchInput}) {
               className={styles.searchInput}
               type='text'
           placeholder='Search'
-          onChange={(e) => handleSearch(e.target.value)}    
-                value={searchInput}
+          onChange={(e) => handleSearch(e.target.value)} 
+          value={query}
           />
-            <div className={styles.suggestionsbox}>
+          {showSuggestions &&   <div className={styles.suggestionsbox}>
     {suggestions.map((title, index) => (
       <div key={index}  className={`${styles['suggestionitem']} ${selectedSuggestion === title ? styles['selected'] : ''}`}
       onClick={() => handleClick(title)}>
         {title}
       </div>
     ))}
-  </div>
+  </div>}
        </div>
       </div>
     )
